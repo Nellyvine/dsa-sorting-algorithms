68e32e0e51fe3e5ddeaf9333a612ff312e3b01ad
Nellyvine <n.mizero@alustudent.com>
2026-09-10 02:44:02 +0400

    simple sorting syntax for both bubble and quick

---
+++ code/quicksort.py
@@ -0,0 +1,15 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return quick_sort(left) + middle + quick_sort(right)
+
+my_list = [38, 27, 43, 3, 9, 82, 10]
+
+sorted_list = quick_sort(my_list)
+print("Sorted List:", sorted_list)
